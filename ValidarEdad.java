d5f1a8b224305e4ebe528de920f2ace4eb7c83c1
angelG146 <angelgoicochea560@gmail.com>
2026-06-03 23:28:31 -0500

    Update ValidarEdad.java

---
+++ ValidarEdad.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ValidarEdad {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int edad;
+
+        while (true) {
+            System.out.print("Ingrese edad: ");
+            edad = sc.nextInt();
+
+            if (edad < 0 || edad > 120) {
+                System.out.println("Edad inválida.");
+                continue;
+            }
+
+            System.out.println("Edad válida.");
+            break;
+        }
+    }
+}
