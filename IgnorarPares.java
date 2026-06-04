bcde1a3ffabbf94a82dc9fa937dfcd57da47ab38
angelG146 <angelgoicochea560@gmail.com>
2026-06-03 23:25:43 -0500

    Update IgnorarPares.java

--- IgnorarPares.java
+++ IgnorarPares.java
@@ -1,17 +1,10 @@
-import java.util.Scanner;
-
 public class IgnorarPares {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("Ingrese un número: ");
-            int num = sc.nextInt();
-
-            if (num < 0) {
-                System.out.println("Número negativo encontrado.");
-                break;
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                continue;
             }
+            System.out.println(i);
         }
     }
 }
