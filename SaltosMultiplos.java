002136f1b1635d345b1fe9db2ea9c5223574247e
angelG146 <angelgoicochea560@gmail.com>
2026-06-03 23:32:32 -0500

    Update SaltosMultiplos.java

---
+++ SaltosMultiplos.java
@@ -0,0 +1,12 @@
+public class SaltosMultiplos {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % 5 == 0) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
